de29f393434f7bab63e88e56546db3e195e585b8
MahimPC111 <mahimpc111@gmail.com>
2022-12-22 21:51:37 +0600

    jwt and stripe committed

--- src/pages/Components/DashBoard/Payment/Checkout.js
+++ src/pages/Components/DashBoard/Payment/Checkout.js
@@ -1,126 +1,126 @@
-// import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-// import React, { useEffect, useState } from 'react';
-// import toast from 'react-hot-toast';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
 
-// const CheckoutForm = ({ order }) => {
-//     const [clientSecret, setClientSecret] = useState("");
-//     const [transactionId, setTransactionId] = useState("");
-//     const [processing, setProcessing] = useState(false);
-//     const stripe = useStripe();
-//     const elements = useElements()
-//     const { _id, price, buyer, email } = order;
+const CheckoutForm = ({ order }) => {
+    const [clientSecret, setClientSecret] = useState("");
+    const [transactionId, setTransactionId] = useState("");
+    const [processing, setProcessing] = useState(false);
+    const stripe = useStripe();
+    const elements = useElements()
+    const { _id, price, buyer, email } = order;
 
-//     useEffect(() => {
-//         fetch("https://recycle-zone-server-ten.vercel.app/create-payment-intent", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json",
-//             },
-//             body: JSON.stringify({ price }),
-//         })
-//             .then((res) => res.json())
-//             .then((data) => setClientSecret(data.clientSecret));
-//     }, [price]);
+    useEffect(() => {
+        fetch("https://recycle-zone-server-ten.vercel.app/create-payment-intent", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ price }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret));
+    }, [price]);
 
-//     const handleSubmit = async (event) => {
-//         event.preventDefault();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-//         if (!stripe || !elements) {
-//             return;
-//         }
+        if (!stripe || !elements) {
+            return;
+        }
 
-//         const card = elements.getElement(CardElement);
+        const card = elements.getElement(CardElement);
 
-//         if (card === null) {
-//             return;
-//         }
+        if (card === null) {
+            return;
+        }
 
-//         const { error, paymentMethod } = await stripe.createPaymentMethod({
-//             type: 'card',
-//             card
-//         });
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
 
-//         if (error) {
-//             toast.error(error.message);
-//         }
+        if (error) {
+            toast.error(error.message);
+        }
 
-//         setProcessing(true);
+        setProcessing(true);
 
-//         const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
-//             clientSecret,
-//             {
-//                 payment_method: {
-//                     card: card,
-//                     billing_details: {
-//                         name: buyer,
-//                         email: email
-//                     },
-//                 },
-//             },
-//         );
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        name: buyer,
+                        email: email
+                    },
+                },
+            },
+        );
 
-//         if (confirmError) {
-//             toast.error(confirmError.message)
-//             return;
-//         }
+        if (confirmError) {
+            toast.error(confirmError.message)
+            return;
+        }
 
-//         if (paymentIntent.status === "succeeded") {
-//             console.log('card info', card)
+        if (paymentIntent.status === "succeeded") {
+            console.log('card info', card)
 
-//             const payment = {
-//                 orderId: _id,
-//                 transactionId: paymentIntent.id,
-//                 price,
-//                 buyer,
-//                 email,
-//             }
+            const payment = {
+                orderId: _id,
+                transactionId: paymentIntent.id,
+                price,
+                buyer,
+                email,
+            }
 
-//             fetch('https://recycle-zone-server-ten.vercel.app/payments', {
-//                 method: 'POST',
-//                 headers: {
-//                     'content-type': 'application/json',
-//                 },
-//                 body: JSON.stringify(payment)
-//             })
-//                 .then(res => res.json())
-//                 .then(data => {
-//                     if (data.acknowledged) {
-//                         toast.success('Your payment is successfully completed')
-//                         setTransactionId(paymentIntent.id)
-//                     }
-//                 })
-//         }
-//         setProcessing(false);
-//     }
+            fetch('https://recycle-zone-server-ten.vercel.app/payments', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json',
+                },
+                body: JSON.stringify(payment)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.acknowledged) {
+                        toast.success('Your payment is successfully completed')
+                        setTransactionId(paymentIntent.id)
+                    }
+                })
+        }
+        setProcessing(false);
+    }
 
-//     return (
-//         <>
-//             <form onSubmit={handleSubmit}>
-//                 <CardElement
-//                     options={{
-//                         style: {
-//                             base: {
-//                                 fontSize: '16px',
-//                                 color: '#424770',
-//                                 '::placeholder': {
-//                                     color: '#aab7c4',
-//                                 },
-//                             },
-//                             invalid: {
-//                                 color: '#9e2146',
-//                             },
-//                         },
-//                     }}
-//                 />
-//                 <button className='btn btn-sm mt-6' type="submit" disabled={!stripe || !clientSecret || processing}>
-//                     Pay
-//                 </button>
-//             </form>
-//             {
-//                 transactionId && <p>Your transaction ID is: <span className='font-bold'>{transactionId}</span></p>
-//             }
-//         </>
-//     );
-// };
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                <button className='btn btn-sm mt-6' type="submit" disabled={!stripe || !clientSecret || processing}>
+                    Pay
+                </button>
+            </form>
+            {
+                transactionId && <p>Your transaction ID is: <span className='font-bold'>{transactionId}</span></p>
+            }
+        </>
+    );
+};
 
-// export default CheckoutForm;
+export default CheckoutForm;

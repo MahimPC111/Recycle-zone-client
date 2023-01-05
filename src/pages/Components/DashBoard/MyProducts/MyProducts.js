95bd189418884b2cf2b46d70766847cbc3d42e6c
MahimPC111 <mahimpc111@gmail.com>
2023-01-05 12:52:48 +0600

    products commtted

--- src/pages/Components/DashBoard/MyProducts/MyProducts.js
+++ src/pages/Components/DashBoard/MyProducts/MyProducts.js
@@ -80,7 +80,7 @@ const MyProducts = () => {
                                     <td>{product.name}</td>
                                     <td>{product.resale_price}</td>
                                     <td>{product.status}</td>
-                                    <td><button onClick={() => handleAdvertise(product._id)} className='btn btn-sm btn-primary' disabled={product.isAdvertised}>
+                                    <td><button onClick={() => handleAdvertise(product._id)} className='btn btn-sm btn-primary' disabled={product.isAdvertised || 'sold'}>
                                         {product.isAdvertised ? 'Advertised' : 'Advertise'}
                                     </button></td>
                                     <td><button onClick={() => handleDelete(product._id)} className='btn btn-sm btn-error'>X</button></td>
